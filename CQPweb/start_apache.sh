0d5f47ef82b34fc203b3328f9bfaa144cac87cc4
Mike Lynch <mlynch@sydney.edu.au>
2022-05-13 12:24:31 +1000

    Builds symlinks to existing corpora in the start_apache.sh script

--- CQPweb/start_apache.sh
+++ CQPweb/start_apache.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+for regfile in ${REGISTRY_VOL}/*; do
+	target="${WEB_ROOT}/CQPweb/${regfile##*/}"
+	if ! [[ $regfile =~ __freq$ || -e $target ]]
+	then 
+		echo "linking corpus ${target}"
+		ln -s ${WEB_ROOT}/CQPweb/exe $target
+	fi
+done 
+
 
 #/var/www/html, where CQPweb sits
 chown -R ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} /var/www/html
